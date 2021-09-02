4fc030e90a22aa9ec884ec88259d6f014f04b604
d-lashgari <68376494+d-lashgari@users.noreply.github.com>
2021-09-02 15:57:05 +0200

    Add files via upload
    Reference Scenario

---
+++ SC0/bcr.h
@@ -0,0 +1,45 @@
+#ifndef BCR_H
+#define BCR_H
+#include <vector>
+#include "parameters.h"
+#include <string>
+void initialize_Seeds (parameters &p);
+
+struct mutation_array
+{
+    int silent =0;
+    int beneficial=0;
+    int harmful=0;
+    int key_mutations_beneficial=0;
+    int lethal=0;
+
+};
+class BCR
+{
+    //This class includes the implementation of B-cell receptor
+public:
+    //Shape-space method
+    BCR(parameters &p);
+    vector<int> BCReceptor;
+    double pMut; // Probability of mutation
+    mutation_array nMutFromGermline; // Number of mutations from the beginning founder cell
+    void mutateBCR(parameters &p);
+    double getMyAffinity4Ag(parameters& p);
+    long double k_on;
+    long double k_off;
+    double distance;
+    string print_BCR();
+    bool checked_initial_dissociation;
+    void calculate_koff(parameters &p);
+    void calculate_on_off_P(parameters &p);
+    double P_on;
+    double P_off;
+    double m_theta; // This angle  is used to specify the contribution of Pon/Poff according to each mutational distance.
+    double delta_Pon;
+    double delta_Poff;
+    double delta_mtheta;
+    double delta_Affinity;
+};
+
+
+#endif // BCR_H
---
+++ SC0/random.h
@@ -0,0 +1,63 @@
+#ifndef i_random
+#define i_random
+#include <random>
+#include <vector>
+using namespace std;
+extern std::mt19937 generator;
+
+struct random
+{
+//    static std::mt19937* generator;
+//    static bool initialized;
+//    static void initializeRandom(unsigned int init_seed=0);
+    static int randomInteger (int); // careful, borders are reached
+    static int randomInteger(int a,int b);
+    static double randomDouble (double);
+    static double randomDouble(double fMin, double fMax);
+    static int randomNormalint (int a,int b);
+    static double randomWeibull(double a, double b);
+    static double randomNormaldouble (double a, double b);
+
+
+    static double cell_cycle_time (double,int);
+//    static double randomGaussian(double, double);
+//    static double boundedRandomGaussian(double mean , double SD, double vmin, double vmax);
+//    template<typename T>
+//
+////    static void shuffle(vector<T> v){
+////        if(!initialized) initializeRandom();
+////        std::shuffle(v.begin(), v.end(), *generator);
+////    }
+//
+//    void shufsfle(vector<double>);
+//    void shuffle(vector<int>);
+};
+double inverse_erf(double x);
+
+class gauss_randomize {
+public:
+    gauss_randomize();
+    gauss_randomize(short dataset);
+    ~gauss_randomize();
+    double get_distribution_value();
+private:
+    void gauss_initialize(double, double, int, double, double);
+    double gauss(double&,double&,double&);
+    void cyster_initialize();
+    double cyster07angle_wt(int angle);
+    vector <double> field;
+    int arraydim;
+    
+    static const double Ny;
+};
+/*class permutations {
+  public:
+   permutations(int dim);
+   vector<int> get_rand_permutation();
+  private:
+   int n_permut;
+   int dimension;
+   vector< vector <int> > savedPermutations;
+};*/
+
+#endif
